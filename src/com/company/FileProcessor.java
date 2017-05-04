efe4a3cd6f5e933a4f3d4c81d628b0381227f014
Darkfree <darkfree97@gmail.com>
2017-05-04 15:28:43 +0300

    Final edition v1.0

--- src/com/company/FileProcessor.java
+++ src/com/company/FileProcessor.java
@@ -1,9 +1,17 @@
 package com.company;
 
 import com.company.transport.Run;
+import com.company.transport.bicycle.ChildBicycle;
+import com.company.transport.bicycle.SimpleBicycle;
 import com.google.gson.Gson;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -61,5 +69,79 @@ public class FileProcessor {
             e.printStackTrace();
         }
         return successFlag;
+    }
+
+    public Object readObjectFromJSON(String fileName){
+        List<Run> runs = new ArrayList<>();
+        Object object = null;
+        FileReader fileReader;
+
+        String buffer = "";
+        try {
+            fileReader = new FileReader(fileName+".json");
+            while (fileReader.ready()){
+                buffer += (char)fileReader.read();
+            }
+            JSONParser parser = new JSONParser();
+            JSONArray jsonArray = (JSONArray)parser.parse(buffer);
+            jsonArray.forEach(p-> {
+                JSONObject o = ((JSONObject)p);
+                if(o.containsKey("childsAge")){
+                    runs.add(new ChildBicycle());
+                    //runs.add(new ChildBicycle((int)o.get("currentSpeed"),(double)o.get("mass"),(double)o.get("wheelDiametr"),(boolean)o.get("bell"),(boolean)o.get("headlights"),(boolean)o.get("amortization"),(boolean)o.get("electricDrive"),(int)o.get("countMotionTransmissionMeans"),(int)o.get("childsAge"),(boolean)o.get("parentalControlCnob")));
+                }
+                if(o.containsKey("brakes")){
+                    runs.add(new SimpleBicycle());
+                }
+                if(o.containsKey("")){
+
+                }
+                if(o.containsKey("")){
+
+                }
+            });
+//            object = runs;
+            object = (List<Run>) jsonArray;
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }catch (ClassCastException e){
+            System.err.println("Casting exception in FileProcessor.readObjectFromJSON");
+        }
+        return object;
+    }
+
+    public void writeToFile(String stringForWrite, String fileName){
+        writeToFile(stringForWrite,fileName,false);
+    }
+
+    public void writeToFile(String stringForWrite, String fileName, boolean append){
+        try {
+            FileWriter fileWriter = new FileWriter(fileName,true);
+            fileWriter.write(stringForWrite);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readFile(String fileName){
+        String buffer = "";
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            while (fileReader.ready()){
+                buffer += (char) fileReader.read();
+            }
+            fileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(buffer);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ import com.company.transport.bicycle.EBrakes;
 import com.company.transport.bicycle.SimpleBicycle;
 import com.company.transport.rocket.CosmicProbe;
 import com.company.transport.rocket.Satellite;
+import org.json.simple.JSONObject;
 
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,56 +19,69 @@ public class Main {
     public static void main(String[] args) {
         FileProcessor fileProcessor = new FileProcessor();
         List<Run> runs = new ArrayList<>();
-        Run runSimpleBicycle = new SimpleBicycle(15,20.0,76,true,false,true,false,2, EBrakes.HydraulicDisc);
-        runs.add(runSimpleBicycle);
-        Run runChildBicycle = new ChildBicycle(5,5.0,20,true,true,true,false,3,4,true);
-        runs.add(runChildBicycle);
-        Run runSatellite = new Satellite(3000,50000,"Solar","Gold Titanium","Reactive",12,0,"Communication",true);
-        runs.add(runSatellite);
-        Run runCosmicProbe = new CosmicProbe(10000,30000,"Plutonium","Platinum","Reactive",5,true);
-        runs.add(runCosmicProbe);
-//        List<Run> finalRuns = runs;
-//        runs.forEach(item->{
-//            System.out.println("Виведення об'єкта: "+item);
-//            System.out.println("Ім'я транспорту: "+item.getTransportName());
-//            System.out.println("Кількість засобів пересування: "+item.getCountMotionTransmissionMeans());
-//            System.out.println("Швидкість пересування: "+item.getSpeed());
-//            System.out.println("Маса транспорту: "+item.getMass());
-//            System.out.println("Кількість пасажирів: "+item.getCountPassengers());
-//            System.out.println("Хеш код: "+item.hashCode());
-//            System.out.print(item.getTransportName()+".equals(): | ");
-////            finalRuns.forEach(run -> System.out.print("With "+run.getTransportName()+" = "+item.equals(run)+" | "));
-//            System.out.println("\n____________________________________________________________\n");
-//        });
 
-        fileProcessor.writeObjectToJSON(runs,"runs");
-//        fileProcessor.writeSerializeObjectIntoFile(runs,"velo.txt");
-        runs = (List<Run>) fileProcessor.readSerializeObjectFromFile("velo.txt");
+//        // Створення та ініціалізація списків об'єктів
+//        List<SimpleBicycle> simpleBicycles = new ArrayList<>();
+//        List<ChildBicycle> childBicycles = new ArrayList<>();
+//        List<CosmicProbe> cosmicProbes = new ArrayList<>();
+//        List<Satellite> satellites = new ArrayList<>();
+//
+//        // Створення обєктів та занесення їх до відповідних масивів
+//        simpleBicycles.add(new SimpleBicycle(15,20.0,76,true,false,true,false,2, EBrakes.HydraulicDisc));
+//        simpleBicycles.add(new SimpleBicycle(20,15.0,80,true,true,true,true,2, EBrakes.MechanicDisk));
+//        childBicycles.add(new ChildBicycle(5,5.0,20,true,true,true,false,3,4,true));
+//        childBicycles.add(new ChildBicycle(12,10.0,50,true,false,true,false,2,8,false));
+//        cosmicProbes.add(new CosmicProbe(10000,30000,"Plutonium","Platinum","Reactive",5,true));
+//        cosmicProbes.add(new CosmicProbe(12683,23464,"Solar Wind","Gold","Reactive",15,true));
+//        satellites.add(new Satellite(3000,50000,"Solar","Gold Titanium","Reactive",12,0,"Communication",true));
+//        satellites.add(new Satellite(6483,23842,"Hydrogen Oxygen","Titanium","Reactive",12,5,"Since",true));
+//
+//        // Запис масивів об'єктів у файли
+//        fileProcessor.writeSerializeObjectIntoFile(simpleBicycles,"simple_bicycles.dat");
+//        fileProcessor.writeSerializeObjectIntoFile(childBicycles,"child_bicycles.dat");
+//        fileProcessor.writeSerializeObjectIntoFile(cosmicProbes,"cosmic_probes.dat");
+//        fileProcessor.writeSerializeObjectIntoFile(satellites,"satellites.dat");
 
-        try {
-            FileWriter fileWriter = new FileWriter("simple_bicycle.txt",true);
-            fileWriter.write("Hello World\n");
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Читання об'єктів з файлів
+        runs.addAll((List<Run>)fileProcessor.readSerializeObjectFromFile("simple_bicycles.dat"));
+        runs.addAll((List<Run>)fileProcessor.readSerializeObjectFromFile("child_bicycles.dat"));
+        runs.addAll((List<Run>)fileProcessor.readSerializeObjectFromFile("cosmic_probes.dat"));
+        runs.addAll((List<Run>)fileProcessor.readSerializeObjectFromFile("satellites.dat"));
 
-        try {
-            FileReader fileReader = new FileReader("simple_bicycle.txt");
-            while (fileReader.ready()){
-                System.out.println((char) fileReader.read());
-            }
-            fileReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // Виведення об'єктів в консоль
+        List<Run> finalRuns = runs;
+        runs.forEach(item->{
+            System.out.println("Виведення об'єкта: "+item);
+            System.out.println("Ім'я транспорту: "+item.getTransportName());
+            System.out.println("Кількість засобів пересування: "+item.getCountMotionTransmissionMeans());
+            System.out.println("Швидкість пересування: "+item.getSpeed());
+            System.out.println("Маса транспорту: "+item.getMass());
+            System.out.println("Кількість пасажирів: "+item.getCountPassengers());
+            System.out.println("Хеш код: "+item.hashCode());
+            System.out.print(item.getTransportName()+".equals(): | ");
+            finalRuns.forEach(run -> System.out.print("With "+run.getTransportName()+" = "+item.equals(run)+" | "));
+            System.out.println("\n____________________________________________________________\n");
+        });
 
+        // Сортування об'єктів
+        System.out.println("\n_-_-_-_-_-_ Не відсортований список _-_-_-_-_-_\n");
         runs.forEach(p-> System.out.println(p.getTransportName()));
-        System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+        System.out.println("\n-_-_-_-_-_- Відсортований  список -_-_-_-_-_-_-\n");
         Collections.sort(runs);
         runs.forEach(p-> System.out.println(p.getTransportName()));
+
+
+//        // Читання чапис обєктів способом серіалізації десеріалізації
+//        fileProcessor.writeSerializeObjectIntoFile(runs,"runs.dat");
+//        runs = (List<Run>) fileProcessor.readSerializeObjectFromFile("runs.dat");
+//
+//        // Читання запис текстового файлу
+//        fileProcessor.writeToFile("Hello World","Hello_World.txt");
+//        fileProcessor.readFile("Hello_World.txt");
+//
+//        // Читання запис JSON
+//        fileProcessor.writeObjectToJSON(runs,"runs");
+//        ((List<JSONObject>)fileProcessor.readObjectFromJSON("runs")).forEach(p-> System.out.println(p));
     }
 
 }
